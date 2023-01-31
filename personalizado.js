22db42f435034ce0ddb47482947e4ad537241eb0
Elton <elton-diego@live.com>
2023-01-31 13:26:41 -0300

    js-adicionado

---
+++ personalizado.js
@@ -0,0 +1,60 @@
+
+let card1 = document.getElementById("card1")
+let card2 = document.getElementById("card2")
+let card3 = document.getElementById("card3")
+
+
+
+// manipulação do card 1
+
+function cardfundo(){
+   document.getElementById("card1").style.backgroundColor = "red"
+  
+}   
+
+
+function cardfundo1(){
+    document.getElementById("card1").style.backgroundColor = "#A299AD"
+    
+
+}
+
+
+
+//manipulação card 2
+
+function cardfundo2(){
+    document.getElementById("card2").style.backgroundColor = "red"
+}
+
+function cardfundo21(){
+
+    document.getElementById("card2").style.backgroundColor = "#A299AD"
+
+}
+
+
+
+//manipulação card 3
+
+function cardfundo3(){
+    document.getElementById("card3").style.backgroundColor = "red"
+
+}
+
+function cardfundo22(){
+    document.getElementById("card3").style.backgroundColor = "#A299AD"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
